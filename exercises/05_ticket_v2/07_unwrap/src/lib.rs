0891a8f32fc3bb00b3ce8df10212d3138af91224
vivek2584 <vivekroy2584@gmail.com>
2025-06-14 15:24:48 +0000

    soln. 05 -> 07

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -1,8 +1,19 @@
+use core::panic;
+
 // TODO: `easy_ticket` should panic when the title is invalid.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(error) => {
+            if error.contains("Description") {
+                Ticket::new(title, "Description not provided".to_string(), status).unwrap()
+            } else {
+                panic!("{error}");
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -71,5 +82,3 @@ mod tests {
         assert_eq!(ticket.description, "Description not provided");
     }
 }
-
-//
